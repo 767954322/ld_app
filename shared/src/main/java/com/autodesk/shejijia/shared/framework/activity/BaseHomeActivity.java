5b984b15f567d0ce614b9fd283d299f6d6da70e8
Wenhui Lin <linwenhui46@gmail.com>
2016-07-12 11:12:58 +0800

    [Refactor]remove radio button list in home activity

--- shared/src/main/java/com/autodesk/shejijia/shared/framework/activity/BaseHomeActivity.java
+++ shared/src/main/java/com/autodesk/shejijia/shared/framework/activity/BaseHomeActivity.java
@@ -49,8 +49,6 @@ public class BaseHomeActivity extends NavigationBarActivity implements RadioGrou
         mTvMsgNumber = (TextView) findViewById(R.id.tv_msg_number);
         mDesignerSessionRadioBtn = (RadioButton) findViewById(getDesignerSessionRadioBtnId());
         mRadioGroup = (RadioGroup) findViewById(getRadioGroupId());
-
-        addRadioButtons(mDesignerSessionRadioBtn);
     }
 
 
@@ -109,10 +107,6 @@ public class BaseHomeActivity extends NavigationBarActivity implements RadioGrou
         super.onDestroy();
     }
 
-    protected void addRadioButtons(RadioButton button) {
-        mRadioButtons.add(button);
-    }
-
     protected RadioButton getRadioButtonById(int id) {
         RadioButton button = null;
         if (id == getDesignerSessionRadioBtnId()) {
@@ -141,8 +135,6 @@ public class BaseHomeActivity extends NavigationBarActivity implements RadioGrou
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-        setRadioButtonChecked(checkedId);
-
         if (needLoginOnRadiobuttonTap(checkedId))
             startRegisterOrLoginActivity(checkedId);
     }
@@ -248,19 +240,6 @@ public class BaseHomeActivity extends NavigationBarActivity implements RadioGrou
         startActivity(intent);
     }
 
-
-    private void setRadioButtonChecked(int id) {
-        RadioButton checkedButton = getRadioButtonById(id);
-
-        for (int i = 0; i < mRadioButtons.size(); ++i) {
-            RadioButton button = mRadioButtons.get(i);
-            if (checkedButton == button)
-                button.setTextColor(UIUtils.getColor(R.color.bg_tab_pressed));
-            else
-                button.setTextColor(UIUtils.getColor(R.color.bg_tab_normal));
-        }
-    }
-
     private void registerBroadcastReceiver() {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BroadCastInfo.RECEVIER_RECEIVERMESSAGE);
@@ -317,8 +296,6 @@ public class BaseHomeActivity extends NavigationBarActivity implements RadioGrou
     }
 
     private RadioButton mDesignerSessionRadioBtn;
-    private List<RadioButton> mRadioButtons = new ArrayList<RadioButton>();
-
     private RadioGroup mRadioGroup;
     private TextView mTvMsgNumber;
 
