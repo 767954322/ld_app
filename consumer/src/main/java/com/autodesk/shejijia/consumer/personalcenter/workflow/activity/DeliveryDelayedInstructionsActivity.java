5a06c5c514a93f27c5d72067f7cf72556b042144
liuhea <he.liu@leediancn.com>
2016-07-26 11:06:19 +0800

    增加交付物说明页面，增加交付物确认延期按钮

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/workflow/activity/DeliveryDelayedInstructionsActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/workflow/activity/DeliveryDelayedInstructionsActivity.java
@@ -7,9 +7,6 @@ import com.autodesk.shejijia.consumer.R;
 import com.autodesk.shejijia.shared.components.common.utility.UIUtils;
 import com.autodesk.shejijia.shared.framework.activity.NavigationBarActivity;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 /**
  * @author he.liu .
  * @version v1.0 .
@@ -35,27 +32,6 @@ public class DeliveryDelayedInstructionsActivity extends NavigationBarActivity {
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
         setTitleForNavbar(UIUtils.getString(R.string.title_introduce));
-        mTvDelayedInstructions.setText(getFromAssets("delivery_delayed_instructions.txt"));
-    }
-
-    /**
-     * 读取资源文件
-     *
-     * @param fileName 　文件路径
-     * @return 返回的文本的内容
-     */
-    public String getFromAssets(String fileName) {
-        String result = "";
-        try {
-            InputStreamReader inputReader = new InputStreamReader(getResources().getAssets().open(fileName));
-            BufferedReader bufReader = new BufferedReader(inputReader);
-            String line = "";
-            while ((line = bufReader.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        mTvDelayedInstructions.setText(UIUtils.getString(R.string.delivery_delayed_instructions));
     }
 }
