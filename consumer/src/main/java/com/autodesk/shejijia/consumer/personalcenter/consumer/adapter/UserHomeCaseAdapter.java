d2b4e954cc3d73e9b3230fef415a2751dd3835b2
yangxuewuWillson <xuewu.yang@leediancn.com>
2016-08-02 14:19:44 +0800

    优化界面

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/adapter/UserHomeCaseAdapter.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/adapter/UserHomeCaseAdapter.java
@@ -133,11 +133,11 @@ public class UserHomeCaseAdapter extends BaseAdapter<CaseLibraryBean.CasesEntity
         MemberEntity mMemberEntity = AdskApplication.getInstance().getMemberEntity();
         if (mMemberEntity != null && Constant.UerInfoKey.DESIGNER_TYPE.equals(mMemberEntity.getMember_type())) {
             ((ViewHolder) holder).imgConsumeChat.setVisibility(View.GONE);
-            ((ViewHolder) holder).mLine.setVisibility(View.GONE);
+           // ((ViewHolder) holder).mLine.setVisibility(View.GONE);
 
         } else {
             ((ViewHolder) holder).imgConsumeChat.setVisibility(View.VISIBLE);
-            ((ViewHolder) holder).mLine.setVisibility(View.VISIBLE);
+           // ((ViewHolder) holder).mLine.setVisibility(View.VISIBLE);
         }
 
         ((ViewHolder) holder).ivHeadIcon.setOnClickListener(new MyOnClickListener(position, ((ViewHolder) holder)));
