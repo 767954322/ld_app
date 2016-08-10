4ea9e2e9d24746aa684f5609b32bc84029648ff5
xueqiudong <xueqiu.dong@leediancn.com>
2016-08-10 11:18:02 +0800

    设计师列表添加筛选和搜索按钮及页面跳转

--- consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationdesigners/activity/DesignerSearchActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationdesigners/activity/DesignerSearchActivity.java
@@ -3,6 +3,8 @@ package com.autodesk.shejijia.consumer.home.decorationdesigners.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.autodesk.shejijia.consumer.R;
+
 public class DesignerSearchActivity extends AppCompatActivity {
 
     @Override
