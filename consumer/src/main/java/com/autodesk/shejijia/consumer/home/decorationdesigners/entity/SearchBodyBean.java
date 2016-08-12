b1397536603bdb54513382009c244ecfe36ee9ab
liuhea <he.liu@leediancn.com>
2016-08-12 13:36:10 +0800

    1.删除无用的json文件；2.增加搜索添加，从业年限，风格，价格json文件及uitils；3.增加搜索逻辑，等待接口联调。

---
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationdesigners/entity/SearchBodyBean.java
@@ -0,0 +1,12 @@
+package com.autodesk.shejijia.consumer.home.decorationdesigners.entity;
+
+/**
+ * <p>Description:</p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: http://www.leediancn.com</p>
+ *
+ * @author he.liu .
+ * @date 2016-08-12.
+ */
+public class SearchBodyBean {
+}
