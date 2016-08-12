027cc6d43c452b5375541984cbb167e2b7a66443
LtdMalidong <lidong.ma@autodesk.com>
2016-08-12 16:33:25 +0800

    修复提现界面绑定后不可点击选择银行得问题

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/designer/activity/WithdrawalActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/designer/activity/WithdrawalActivity.java
@@ -108,14 +108,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
 
                 mStopDemandAlertView.show();
 
-//                ll_withdrawal_replace_bank_card.setVisibility(View.GONE);
-//                tv_withdrawal_cardholder_name.setVisibility(View.GONE);
-//                tv_withdrawal_branch_bank.setVisibility(View.GONE);
-//                tv_withdrawal_bank_card_number.setVisibility(View.GONE);
-//                et_withdrawal_cardholder_name.setVisibility(View.VISIBLE);
-//                et_withdrawal_branch_bank.setVisibility(View.VISIBLE);
-//                et_withdrawal_bank_card_number.setVisibility(View.VISIBLE);
-
                 break;
             case R.id.btn_withdrawal_true:
                 String tv_user_name = getText(tv_withdrawal_cardholder_name);
@@ -161,9 +153,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
      */
     private String isValidateNumber(String isNumber) {
         KLog.d(TAG, isNumber);
-//        if (!StringUtils.isNonNumeric(isNumber)) {
-//            isNumber = "0000000000000000000";
-//        }
         return isNumber;
     }
 
@@ -351,7 +340,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
 
             tv_withdrawal_account_balance.setText("¥" + myPropertyBean.getAmount() + "");
             tv_withdrawal_account.setText("¥" + myPropertyBean.getAmount());
-//            account_user_name = myPropertyBean.getAccount_user_name();
             bank_name = myPropertyBean.getBank_name();
             deposit_card = myPropertyBean.getDeposit_card();
             branch_bank_name = myPropertyBean.getBranch_bank_name();
@@ -362,10 +350,10 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
                 ll_withdrawal_replace_bank_card.setVisibility(View.VISIBLE); // 绑定后显示解绑按钮
                 et_withdrawal_bank_card_number.setVisibility(View.GONE);
                 et_withdrawal_branch_bank.setVisibility(View.GONE);
+                ll_withdrawal_open_account_bank.setEnabled(false);
             }
 
             if (null == account_user_name || account_user_name.equals("")) {
-//                et_withdrawal_cardholder_name.setVisibility(View.VISIBLE);
             } else {
                 tv_withdrawal_cardholder_name.setVisibility(View.VISIBLE);
                 tv_withdrawal_cardholder_name.setText(account_user_name);
@@ -389,7 +377,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
         }
 
         et_withdrawal_branch_bank.setText(branch_bank_name);
-//        et_withdrawal_cardholder_name.setText(account_user_name);
         et_withdrawal_bank_card_number.setText(deposit_card);
     }
 
@@ -451,7 +438,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
     public void onItemClick(Object object, int position) {
         if (object == mStopDemandAlertView && position != AlertView.CANCELPOSITION) {
             sendUnBindBankCard(designer_id, account_user_name, bank_name, branch_bank_name, deposit_card);
-            //mStopDemandAlertView.dismiss();
             return;
         }
     }
@@ -464,7 +450,6 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
     private TextView tv_withdrawal_open_account_bank;
     private TextView tv_withdrawal_bank_card_number;
     private Button btn_withdrawal_true;
-    //    private EditText et_withdrawal_cardholder_name;
     private EditText et_withdrawal_branch_bank;
     private EditText et_withdrawal_bank_card_number;
     private LinearLayout ll_withdrawal_open_account_bank;
