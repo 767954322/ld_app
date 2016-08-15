645feb01129bb5e5d001ec23be02ed61b5618493
liuhea <he.liu@leediancn.com>
2016-08-15 09:51:40 +0800

    update DesignerStyleBean

--- consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationdesigners/entity/DesignerStyleBean.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationdesigners/entity/DesignerStyleBean.java
@@ -1,5 +1,7 @@
 package com.autodesk.shejijia.consumer.home.decorationdesigners.entity;
 
+import java.util.List;
+
 /**
  * @author he.liu .
  * @version v1.0 .
@@ -10,4 +12,59 @@ package com.autodesk.shejijia.consumer.home.decorationdesigners.entity;
 public class DesignerStyleBean {
 
 
+    /**
+     * code : japanese
+     * name : 日式
+     * extension : 日式风格
+     * description : null
+     */
+
+    private List<RelateInformationListBean> relate_information_list;
+
+    public List<RelateInformationListBean> getRelate_information_list() {
+        return relate_information_list;
+    }
+
+    public void setRelate_information_list(List<RelateInformationListBean> relate_information_list) {
+        this.relate_information_list = relate_information_list;
+    }
+
+    public static class RelateInformationListBean {
+        private String code;
+        private String name;
+        private String extension;
+        private String description;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getExtension() {
+            return extension;
+        }
+
+        public void setExtension(String extension) {
+            this.extension = extension;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
 }
