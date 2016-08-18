f19684a7a0335c8c1f441561526ab8a5248b70b9
767954322 <menghao.gu@leediancn.com>
2016-08-18 19:31:50 +0800

    修改DP-2994提现页面，银行卡号输入后建议按UI图上显示成4位一空格的方式

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/designer/activity/WithdrawalActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/designer/activity/WithdrawalActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Selection;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -97,6 +100,7 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
         btn_withdrawal_true.setOnClickListener(this);
         ll_withdrawal_open_account_bank.setOnClickListener(this);
         ll_withdrawal_replace_bank_card.setOnClickListener(this);
+        et_withdrawal_bank_card_number.addTextChangedListener(new MyWatcher());
     }
 
     @Override
@@ -125,7 +129,11 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
 
 //                boolean isName = account_user_name.trim().matches(regex_name);
                 boolean isBank = branch_bank_name.trim().matches(RegexUtil.ADDRESS_REGEX);
-                boolean isBankNum = deposit_card.trim().matches(RegexUtil.PHONE_BLANK);
+                boolean isBankNum = false;
+                if (!TextUtils.isEmpty(deposit_card)) {
+                    isBankNum = deposit_card.replace(" ", "").trim().matches(RegexUtil.PHONE_BLANK);
+                }
+
 //                if (!isName) {
 //                    Toast.makeText(WithdrawalActivity.this, "只能包含2-10位汉字或英文", Toast.LENGTH_SHORT).show();
 //                    break;
@@ -148,7 +156,7 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
 
                 } else {
 
-                    if(!myPropertyBean.getDeposit_card().trim().equals(deposit_card)){
+                    if (!myPropertyBean.getDeposit_card().trim().equals(deposit_card)) {
 
                         if (!isBankNum) {
                             Toast.makeText(WithdrawalActivity.this, "银行卡号请输入16到19位数字", Toast.LENGTH_SHORT).show();
@@ -570,6 +578,100 @@ public class WithdrawalActivity extends NavigationBarActivity implements View.On
         if (object == mStopDemandAlertView && position != AlertView.CANCELPOSITION) {
             sendUnBindBankCard(designer_id, account_user_name, bank_name, branch_bank_name, deposit_card);
             return;
+        }
+    }
+
+
+    class MyWatcher implements TextWatcher {
+        int beforeTextLength = 0;
+        int onTextLength = 0;
+        boolean isChanged = false;
+        int location = 0;// 记录光标的位置
+        private char[] tempChar;
+        private StringBuffer buffer = new StringBuffer();
+        int konggeNumberB = 0;
+
+        @Override
+        public void beforeTextChanged(CharSequence text, int start, int count, int after) {
+
+            // TODO Auto-generated method stub
+            beforeTextLength = text.length();
+            if (buffer.length() > 0) {
+                buffer.delete(0, buffer.length());
+            }
+            konggeNumberB = 0;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) == ' ') {
+                    konggeNumberB++;
+                }
+            }
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence text, int start, int before, int count) {
+
+            // TODO Auto-generated method stub
+            onTextLength = text.length();
+            buffer.append(text.toString());
+            if (onTextLength == beforeTextLength || onTextLength <= 3 || isChanged) {
+                isChanged = false;
+                return;
+            }
+            isChanged = true;
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+            if (isChanged) {
+                location = et_withdrawal_bank_card_number.getSelectionEnd();
+                int index = 0;
+                while (index < buffer.length()) {
+                    if (buffer.charAt(index) == ' ') {
+                        buffer.deleteCharAt(index);
+                    } else {
+                        index++;
+                    }
+                }
+                index = 0;
+                int konggeNumberC = 0;
+                while (index < buffer.length()) {
+
+                    if ((index + 1) % 5 == 0) {// if ((index == 4 || index == 9 || index == 14 || index// == 19)) {
+
+                        buffer.insert(index, ' ');
+                        konggeNumberC++;
+                    }
+                    index++;
+                }
+
+                if (konggeNumberC > konggeNumberB) {
+
+                    location += (konggeNumberC - konggeNumberB);
+                }
+
+                tempChar = new char[buffer.length()];
+
+                buffer.getChars(0, buffer.length(), tempChar, 0);
+
+                String str = buffer.toString();
+
+                if (location > str.length()) {
+
+                    location = str.length();
+
+                } else if (location < 0) {
+
+                    location = 0;
+
+                }
+                et_withdrawal_bank_card_number.setText(str);
+                Editable etable = et_withdrawal_bank_card_number.getText();
+                Selection.setSelection(etable, location);
+                isChanged = false;
+            }
+
         }
     }
 
