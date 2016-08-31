684362ba796a8d77a006cb9c5505bea728154b40
yangxuewuWillson <xuewu.yang@leediancn.com>
2016-08-31 21:32:08 +0800

    解决3.1环境bug

--- consumer/src/main/java/com/autodesk/shejijia/consumer/manager/MPServerHttpManager.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/manager/MPServerHttpManager.java
@@ -1000,7 +1000,7 @@ public class MPServerHttpManager {
      * @param callback
      */
     public void ifIsLohoDesiner(String designers, final String hs_uid, OkJsonRequest.OKResponseCallback callback) {
-        String url = UrlConstants.MAIN_MEMBER + "/member-app/v1/api/designers/" + designers;
+        String url = UrlConstants.MAIN_MEMBER + "/designers/" + designers;
         OkJsonRequest okRequest = new OkJsonRequest(OkJsonRequest.Method.GET, url, null, callback) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
