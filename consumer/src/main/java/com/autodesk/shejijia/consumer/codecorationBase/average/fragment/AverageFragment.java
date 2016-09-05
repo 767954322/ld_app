33674fe485c65aa597b2a74c87fa96b7b45b4f4e
liuhea <he.liu@leediancn.com>
2016-09-06 00:12:27 +0800

    修复DP-4479【Android-认证】：未认证设计师在应标大厅应标，提示文字并没有告诉用户要如何进行实名认证，不知道如何继续操作

--- consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/average/fragment/AverageFragment.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/average/fragment/AverageFragment.java
@@ -1,6 +1,5 @@
 package com.autodesk.shejijia.consumer.codecorationBase.average.fragment;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
