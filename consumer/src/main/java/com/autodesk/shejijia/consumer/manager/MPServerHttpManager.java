02830d67182392f80f7d85101681eec789a920e5
LtdLuchongbin <chongbin.lu@autodesk.com>
2016-09-06 13:16:06 +0800

    修复设计师订单列表加载更多不正确的问题

--- consumer/src/main/java/com/autodesk/shejijia/consumer/manager/MPServerHttpManager.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/manager/MPServerHttpManager.java
@@ -762,17 +762,29 @@ public class MPServerHttpManager {
      * 精选订单
      */
     public void getSliteOder(HashMap<String, Object> map, final OkJsonRequest.OKResponseCallback callback) {
+//        {@"offset"   :offset,
+//            @"limit"    :limit,
+//            @"sort_by"  :@"date",
+//            @"sort_order":@"desc",
+//            @"version":version,
+//            @"service_modlue":servicemodlue,
+//            @"node_ids":@"",
+//            @"sub_node_ids":@"",
+//            @"commend":@""};
+
+
+
 
         String url = UrlConstants.URL_GET_ORDER + map.get(JsonConstants.JSON_MEASURE_FORM_DESIGNER_ID) + "/orders?" +
                 "offset=" + map.get(JsonConstants.JSON_DEMAND_LIST_OFFSET) +
                 "&limit=" + map.get(JsonConstants.JSON_DEMAND_LIST_LIMIT) +
-                "&sort_by=date" +
-                "&sort_order=desc" +
+                "&sort_by=desc" +
+                "&sort_order=date" +
                 "&version=2" +
                 "&service_modlue=5" +
-                "&node_ids=1" +
-                "&sub_node_ids=1" +
-                "&commend=5";
+                "&node_ids=" +""+
+                "&sub_node_ids=" +""+
+                "&commend="+"";
 
         OkJsonRequest okRequest = new OkJsonRequest(OkJsonRequest.Method.GET, url, null, callback) {
             @Override
