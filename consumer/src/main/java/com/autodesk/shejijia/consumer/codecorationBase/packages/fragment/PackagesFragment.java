0c83dc72a058a9e74d32950a49004b37b8e8dbbb
yangxuewuWillson <xuewu.yang@leediancn.com>
2016-09-07 11:04:15 +0800

    更改套餐图片

--- consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/packages/fragment/PackagesFragment.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/packages/fragment/PackagesFragment.java
@@ -9,7 +9,6 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.autodesk.shejijia.consumer.R;
 import com.autodesk.shejijia.consumer.codecorationBase.packages.activity.PackageDetailActivity;
@@ -23,17 +22,8 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 /**
  * 套餐
  */
-public class PackagesFragment extends BaseFragment implements View.OnClickListener {
+public class PackagesFragment extends BaseFragment implements View.OnClickListener, GridView.OnItemClickListener {
 
-
-    private ImageView iv_packages_one;
-    private ImageView iv_packages_two;
-    private ImageView iv_packages_three;
-    private ImageView iv_packages_four;
-    private ImageView iv_packages_five;
-    private ImageView iv_packages_six;
-    private ImageView iv_packages_seven;
-    private ImageView iv_packages_eight;
 
     @Override
     protected int getLayoutResId() {
@@ -43,36 +33,22 @@ public class PackagesFragment extends BaseFragment implements View.OnClickListen
     @Override
     protected void initView() {
 
-//        gv_packages = (GridView) rootView.findViewById(R.id.gv_packages);
+        gv_packages = (GridView) rootView.findViewById(R.id.gv_packages);
         bt_packages_yuyue = (ImageButton) rootView.findViewById(R.id.bt_packages_yuyue);
         iv_packages_tital = (ImageView) rootView.findViewById(R.id.iv_packages_tital);
-        iv_packages_one = (ImageView) rootView.findViewById(R.id.iv_packages_one);
-        iv_packages_two = (ImageView) rootView.findViewById(R.id.iv_packages_two);
-        iv_packages_three = (ImageView) rootView.findViewById(R.id.iv_packages_three);
-        iv_packages_four = (ImageView) rootView.findViewById(R.id.iv_packages_four);
-        iv_packages_five = (ImageView) rootView.findViewById(R.id.iv_packages_five);
-        iv_packages_six = (ImageView) rootView.findViewById(R.id.iv_packages_six);
-        iv_packages_seven = (ImageView) rootView.findViewById(R.id.iv_packages_seven);
-        iv_packages_eight = (ImageView) rootView.findViewById(R.id.iv_packages_eight);
 
     }
 
     @Override
     protected void initData() {
 
+        isLoginUserJust = isLoginUser();
         packages = ImageUrlUtils.getPackagesListImage();
         banner_image = ImageUrlUtils.getPackagesListBanner();
 
-        isLoginUserJust = isLoginUser();
         ImageLoader.getInstance().displayImage(banner_image, iv_packages_tital);
-        ImageLoader.getInstance().displayImage(packages[0], iv_packages_one);
-        ImageLoader.getInstance().displayImage(packages[1], iv_packages_two);
-        ImageLoader.getInstance().displayImage(packages[2], iv_packages_three);
-        ImageLoader.getInstance().displayImage(packages[3], iv_packages_four);
-        ImageLoader.getInstance().displayImage(packages[4], iv_packages_five);
-        ImageLoader.getInstance().displayImage(packages[5], iv_packages_six);
-        ImageLoader.getInstance().displayImage(packages[6], iv_packages_seven);
-        ImageLoader.getInstance().displayImage(packages[7], iv_packages_eight);
+        PacksAdapter pagerAdapter = new PacksAdapter();
+        gv_packages.setAdapter(pagerAdapter);
 
     }
 
@@ -81,20 +57,12 @@ public class PackagesFragment extends BaseFragment implements View.OnClickListen
     protected void initListener() {
 
         bt_packages_yuyue.setOnClickListener(this);
-        iv_packages_one.setOnClickListener(this);
-        iv_packages_two.setOnClickListener(this);
-        iv_packages_three.setOnClickListener(this);
-        iv_packages_four.setOnClickListener(this);
-        iv_packages_five.setOnClickListener(this);
-        iv_packages_six.setOnClickListener(this);
-        iv_packages_seven.setOnClickListener(this);
-        iv_packages_eight.setOnClickListener(this);
+        gv_packages.setOnItemClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-
 
         switch (v.getId()) {
             case R.id.bt_packages_yuyue:
@@ -106,47 +74,6 @@ public class PackagesFragment extends BaseFragment implements View.OnClickListen
                 } else {
                     AdskApplication.getInstance().doLogin(activity);
                 }
-
-                break;
-            case R.id.iv_packages_one:
-                Intent intent1 = new Intent(activity, PackageDetailActivity.class);
-                intent1.putExtra("item_num", 0);
-                startActivity(intent1);
-                break;
-            case R.id.iv_packages_two:
-                Intent intent2 = new Intent(activity, PackageDetailActivity.class);
-                intent2.putExtra("item_num", 1);
-                startActivity(intent2);
-                break;
-            case R.id.iv_packages_three:
-                Intent intent3 = new Intent(activity, PackageDetailActivity.class);
-                intent3.putExtra("item_num", 2);
-                startActivity(intent3);
-                break;
-            case R.id.iv_packages_four:
-                Intent intent4 = new Intent(activity, PackageDetailActivity.class);
-                intent4.putExtra("item_num", 3);
-                startActivity(intent4);
-                break;
-            case R.id.iv_packages_five:
-                Intent intent5 = new Intent(activity, PackageDetailActivity.class);
-                intent5.putExtra("item_num", 4);
-                startActivity(intent5);
-                break;
-            case R.id.iv_packages_six:
-                Intent intent6 = new Intent(activity, PackageDetailActivity.class);
-                intent6.putExtra("item_num", 5);
-                startActivity(intent6);
-                break;
-            case R.id.iv_packages_seven:
-                Intent intent7 = new Intent(activity, PackageDetailActivity.class);
-                intent7.putExtra("item_num", 6);
-                startActivity(intent7);
-                break;
-            case R.id.iv_packages_eight:
-                Intent intent8 = new Intent(activity, PackageDetailActivity.class);
-                intent8.putExtra("item_num", 7);
-                startActivity(intent8);
                 break;
             default:
                 break;
@@ -157,7 +84,9 @@ public class PackagesFragment extends BaseFragment implements View.OnClickListen
     @Override
     public void onResume() {
         super.onResume();
+
         isLoginUserJust = isLoginUser();
+
     }
 
     //判断该用户是否登陆了
@@ -174,13 +103,58 @@ public class PackagesFragment extends BaseFragment implements View.OnClickListen
         }
     }
 
-    private String[] list_Image;
-    private String banner_image;
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+        Intent intent = new Intent(activity, PackageDetailActivity.class);
+        intent.putExtra("item_num", position);
+        startActivity(intent);
+
+    }
+
+
+    class PacksAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return packages.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return packages[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            MyHolder myHolder;
+            if (convertView == null) {
+                myHolder = new MyHolder();
+                convertView = LayoutInflater.from(activity).inflate(R.layout.fragment_packages_item, null);
+                myHolder.iv_packets = (ImageView) convertView.findViewById(R.id.iv_packages_item);
+                convertView.setTag(myHolder);
+            }
+            myHolder = (MyHolder) convertView.getTag();
+            ImageLoader.getInstance().displayImage(packages[position], myHolder.iv_packets);
+            return convertView;
+        }
+
+        class MyHolder {
+            private ImageView iv_packets;
+        }
+    }
 
     private String[] packages;
-    private ImageButton bt_packages_yuyue;
+    private String banner_image;
     private boolean isLoginUserJust;
+
+    private GridView gv_packages;
     private ImageView iv_packages_tital;
+    private ImageButton bt_packages_yuyue;
 
 }
