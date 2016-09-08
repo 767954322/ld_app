4a8f0c79879135ed19cb1c4e417b2a8b5cac0d09
767954322 <menghao.gu@leediancn.com>
2016-09-08 17:58:44 +0800

    修复套餐详情宽度填充

--- consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/packages/view/ImageUrlUtils.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/codecorationBase/packages/view/ImageUrlUtils.java
@@ -70,3 +70,66 @@ public class ImageUrlUtils {
         return packages_name;
     }
 }
+/**
+ * 新版本接口
+ */
+//    public static String getEvUrl() {
+//        String urlPath = (String) PropUtil.loadAssetsProperties(AdskApplication.getInstance(), ApiManagerV2.PROPERTY_PATH).get("versionName");
+//        switch (urlPath) {
+//            case "A":
+//                return "http://static.gdfcx.net/leedian-static/alpha/images/app/ios/";
+//            case "U":
+//                return "http://static.gdfcx.net/leedian-static/uat/images/app/ios/";
+//            case "P":
+//                return "http://static.shejijia.com/juran-prod-static/images/app/ios/";
+//            default:
+//                return "http://static.shejijia.com/juran-prod-static/images/app/ios/";
+//        }
+//    }
+//
+//    public static String[] getPackagesListImage() {
+//
+//        String url_header = getEvUrl();
+//        String[] packagesList = {
+//                url_header + "list/juranzhuangshidongjiekuaican.jpg",
+//                url_header + "list/juranzhumeixingfujia.jpg",
+//                url_header + "list/juranzhuangshibeishutaocan.jpg",
+//                url_header + "list/juranzhumeiyishujia.jpg",
+//                url_header + "list/juranzhuangshinanyuntaocan.jpg",
+//                url_header + "list/juranzhumeipinzhijia.jpg",
+//                url_header + "list/juranzhuangshixijingtaocan.jpg",
+//                url_header + "list/juranzhumeizunxiangjia.jpg"
+//        };
+//
+//        return packagesList;
+//    }
+//
+//    public static String[] getPackagesDetailImage() {
+//
+//        String url_header = getEvUrl();
+//        String[] packagesDetail = {
+//                url_header + "detail/dongjie.jpg",
+//                url_header + "detail/xingfujia.jpg",
+//                url_header + "detail/beishu.jpg",
+//                url_header + "detail/yishujia.jpg",
+//                url_header + "detail/nanyun.jpg",
+//                url_header + "detail/pinzhijia.jpg",
+//                url_header + "detail/xijing.jpg",
+//                url_header + "detail/zunxiang.jpg"
+//        };
+//        return packagesDetail;
+//    }
+//
+//    public static String getPackagesListBanner() {
+//        String url_header = getEvUrl();
+//
+//        return url_header + "list/banner2x.jpg";
+//    }
+//
+//    public static String[] getPackagesListNames() {
+//        String[] packages_name = {"居然装饰东捷套餐", "居然装饰东捷套餐",
+//                "居然装饰东捷套餐", "居然住美幸福家", "居然装饰北舒套餐",
+//                "居然住美艺术家", "居然装饰南韵套餐", "居然住美品质家"};
+//
+//        return packages_name;
+//    }
