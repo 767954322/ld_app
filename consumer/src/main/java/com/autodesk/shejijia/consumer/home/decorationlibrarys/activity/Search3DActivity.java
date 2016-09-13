85e9f00f83d0c84bb813d2ce6a30779cb7baf4be
yangxuewuWillson <xuewu.yang@leediancn.com>
2016-09-13 19:16:06 +0800

    解决搜索返回 在进入出现文字没消失问题

--- consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationlibrarys/activity/Search3DActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationlibrarys/activity/Search3DActivity.java
@@ -1,8 +1,6 @@
 package com.autodesk.shejijia.consumer.home.decorationlibrarys.activity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -139,6 +137,7 @@ public class Search3DActivity extends NavigationBarActivity implements
                 openPopupWindow(mCetSearchClick.getText().toString());
                 break;
             case R.id.searchc_back:/// 返回.
+                mCetSearchContent.setText("");
                 finish();
                 break;
             default:
