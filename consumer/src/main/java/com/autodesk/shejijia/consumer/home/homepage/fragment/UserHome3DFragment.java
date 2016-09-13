614bf311292ce67e17b56ea7db51ca26d7018512
LtdLuchongbin <chongbin.lu@autodesk.com>
2016-09-13 18:00:36 +0800

    修复2D案例不能刷新的bug

--- consumer/src/main/java/com/autodesk/shejijia/consumer/home/homepage/fragment/UserHome3DFragment.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/homepage/fragment/UserHome3DFragment.java
@@ -592,7 +592,7 @@ public class UserHome3DFragment extends BaseFragment implements UserHome3DCaseAd
     private LinearLayout.LayoutParams mShadeViewLayoutParams;
     private UserHome3DCaseAdapter mAdapter;
     private int mOffset = 0;
-    private int LIMIT = 50;
+    private int LIMIT = 10;
     private int screenWidth, screenHeight;
 
     private String mNickNameConsumer;
