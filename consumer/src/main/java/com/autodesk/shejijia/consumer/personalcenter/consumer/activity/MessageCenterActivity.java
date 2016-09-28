1cca73ba7440a8fcea1e0dcacc675f5178ad7e6f
767954322 <menghao.gu@leediancn.com>
2016-09-28 11:11:12 +0800

    [Bug][DP-3469][【android-消息中心】消息中心缺少编辑、删除功能][Reviewed by：zhipeng . li]

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/activity/MessageCenterActivity.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/activity/MessageCenterActivity.java
@@ -3,18 +3,23 @@ package com.autodesk.shejijia.consumer.personalcenter.consumer.activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.autodesk.shejijia.consumer.R;
-import com.autodesk.shejijia.consumer.home.decorationdesigners.activity.DesignerFiltrateActivity;
+import com.autodesk.shejijia.consumer.home.widget.SwipePullListView;
 import com.autodesk.shejijia.consumer.manager.MPServerHttpManager;
 import com.autodesk.shejijia.consumer.personalcenter.consumer.adapter.MessageCenterAdapter;
 import com.autodesk.shejijia.consumer.personalcenter.consumer.entity.MessageCenterBean;
@@ -22,7 +27,6 @@ import com.autodesk.shejijia.consumer.utils.ApiStatusUtil;
 import com.autodesk.shejijia.shared.components.common.appglobal.Constant;
 import com.autodesk.shejijia.shared.components.common.appglobal.MemberEntity;
 import com.autodesk.shejijia.shared.components.common.network.OkJsonRequest;
-import com.autodesk.shejijia.shared.components.common.uielements.pulltorefresh.PullListView;
 import com.autodesk.shejijia.shared.components.common.uielements.pulltorefresh.PullToRefreshLayout;
 import com.autodesk.shejijia.shared.components.common.utility.GsonUtil;
 import com.autodesk.shejijia.shared.components.common.utility.MPNetworkUtils;
@@ -34,6 +38,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author gumenghao .
@@ -42,10 +47,13 @@ import java.util.List;
  * @file MessageCenterActivity.java  .
  * @brief 消息中心 .
  */
-public class MessageCenterActivity extends NavigationBarActivity implements View.OnClickListener, PullToRefreshLayout.OnRefreshListener {
-
+public class MessageCenterActivity extends NavigationBarActivity implements View.OnClickListener, PullToRefreshLayout.OnRefreshListener, CheckBox.OnCheckedChangeListener, MessageCenterAdapter.DeleteMessage {
 
     private MemberEntity mMemberEntity;
+    private TextView nav_right_textView;
+    private TextView tv_msg_delete_all;
+    private CheckBox cb_msg_delete_all;
+    private RelativeLayout rl_msg_delete_msgs;
 
     @Override
     protected int getLayoutResId() {
@@ -54,12 +62,17 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
 
     @Override
     protected void initView() {
+        rl_msg_delete_msgs = (RelativeLayout) findViewById(R.id.rl_msg_delete_msgs);
         mIbnBack = (ImageButton) findViewById(R.id.nav_left_imageButton);
+        nav_right_textView = (TextView) findViewById(R.id.nav_right_textView);
+        tv_msg_delete_all = (TextView) findViewById(R.id.tv_msg_delete_all);
+        cb_msg_delete_all = (CheckBox) findViewById(R.id.cb_msg_delete_all);
         mPullToRefreshLayout = ((PullToRefreshLayout) findViewById(R.id.refresh_view));
-        mListView = (PullListView) findViewById(R.id.message_center_list_view);
+        mListView = (SwipePullListView) findViewById(R.id.message_center_list_view);
         mFooterView = View.inflate(this, R.layout.view_empty_layout, null);
         mRlEmpty = (RelativeLayout) mFooterView.findViewById(R.id.rl_empty);
         mTvEmptyMessage = (TextView) mFooterView.findViewById(R.id.tv_empty_message);
+
         mIvEmpty = ((ImageView) mFooterView.findViewById(R.id.iv_default_empty));
 
         mListView.addFooterView(mFooterView);
@@ -70,7 +83,9 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
 
         mContext = this;
         setTitleForNavbar(UIUtils.getString(R.string.title_messagecenter));
-
+//        nav_right_textView.setVisibility(View.VISIBLE);
+//        nav_right_textView.setTextColor(Color.BLUE);
+//        nav_right_textView.setText("编辑");
         mMemberEntity = AdskApplication.getInstance().getMemberEntity();
         if (Constant.UerInfoKey.DESIGNER_TYPE.equals(mMemberEntity.getMember_type())) {
             ifIsDesiner = true;
@@ -79,7 +94,8 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
         }
 
         if (messageCenterAdapter == null) {
-            messageCenterAdapter = new MessageCenterAdapter(mContext, mCasesEntities, ifIsDesiner);
+
+            messageCenterAdapter = new MessageCenterAdapter(mContext, mCasesEntities, ifIsDesiner, mListView.getRightViewWidth());
         }
 
         mListView.setAdapter(messageCenterAdapter);
@@ -90,7 +106,12 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
     protected void initListener() {
         super.initListener();
         mIbnBack.setOnClickListener(this);
+        tv_msg_delete_all.setOnClickListener(this);
+        cb_msg_delete_all.setOnCheckedChangeListener(this);
+        nav_right_textView.setOnClickListener(this);
         mPullToRefreshLayout.setOnRefreshListener(this);
+        messageCenterAdapter.setDeleteMessageListener(this);
+
     }
 
     @Override
@@ -99,7 +120,74 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
             case R.id.nav_left_imageButton:
                 finish();
                 break;
+            case R.id.nav_right_textView:
+
+                //编辑
+//                boolean changeStatus_editor = messageCenterAdapter.getEditor() ? false : true;
+//                if (changeStatus_editor) {
+//                    rl_msg_delete_msgs.setVisibility(View.VISIBLE);
+//                } else {
+//                    rl_msg_delete_msgs.setVisibility(View.GONE);
+//                }
+//                messageCenterAdapter.setEditor(changeStatus_editor);
+//                messageCenterAdapter.notifyDataSetChanged();
+
+                break;
+            case R.id.tv_msg_delete_all:
+
+                //删除
+//                Set<Integer> integers = messageCenterAdapter.getmCheckeds();
+//                if (integers.size() > 0) {
+//                    Toast.makeText(MessageCenterActivity.this, "删除:" + integers.size(), Toast.LENGTH_LONG).show();
+//                    Log.d("test", "size:" + integers.size());
+//
+//                } else {
+//                    Toast.makeText(MessageCenterActivity.this, "删除", Toast.LENGTH_LONG).show();
+//                }
+
+                //请求成功后删除本地数据
+//                mCasesEntities.remove(0);
+//                messageCenterAdapter.notifyDataSetChanged();
+
+                break;
         }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (isChecked) {
+
+            messageCenterAdapter.addAll();
+
+        } else {
+
+            messageCenterAdapter.clearChose();
+
+        }
+
+    }
+
+    //删除单条消息
+    @Override
+    public void deleteOneMsg(final int position, MessageCenterBean.MessagesBean messagesBean, final View convertView) {
+
+        MPServerHttpManager.getInstance().deleteMessage(mMemberEntity.getAcs_member_id(), messagesBean.getMessage_id(), new OkJsonRequest.OKResponseCallback() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(mContext, "删除失败了", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                //请求成功后删除本地数据
+                mListView.hiddenRight(convertView);
+                mCasesEntities.remove(position);
+                messageCenterAdapter.notifyDataSetChanged();
+                Toast.makeText(mContext, "删除成功了", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -204,13 +292,11 @@ public class MessageCenterActivity extends NavigationBarActivity implements View
     private final String LOADMORE_STATUS = "loadmore";
     private int offset = 0;
     private int limit = 10;
-    private int screenWidth;
-    private int screenHeight;
     private TextView mTvEmptyMessage;
     private ImageView mIvEmpty;
     private ImageButton mIbnBack;
 
-    private PullListView mListView;
+    private SwipePullListView mListView;
     private MessageCenterAdapter messageCenterAdapter;
     private PullToRefreshLayout mPullToRefreshLayout;
     private List<MessageCenterBean.MessagesBean> mCasesEntities = new ArrayList<MessageCenterBean.MessagesBean>();
--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/adapter/MessageCenterAdapter.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/consumer/adapter/MessageCenterAdapter.java
@@ -7,7 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.autodesk.shejijia.consumer.R;
 import com.autodesk.shejijia.consumer.personalcenter.consumer.entity.MessageCenterBean;
@@ -16,7 +20,12 @@ import com.autodesk.shejijia.shared.components.common.utility.DateUtil;
 import com.autodesk.shejijia.shared.components.common.utility.GsonUtil;
 import com.autodesk.shejijia.shared.components.common.utility.StringUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author gumenghao .
@@ -26,16 +35,44 @@ import java.util.List;
  * @brief 消息中心 .
  */
 public class MessageCenterAdapter extends BaseAdapter {
-
+    private DeleteMessage deleteMessage;
+    private int right_wid;
     private Context mContext;
     private Boolean ifIsDesiner;
+    private boolean isClickEditor = false;
+    private boolean isClickedAll = false;
+
+
     private List<MessageCenterBean.MessagesBean> mCasesEntities;
+    private final Set<Integer> mCheckeds;
 
 
-    public MessageCenterAdapter(Context mContext, List<MessageCenterBean.MessagesBean> mCasesEntities, Boolean ifIsDesiner) {
+    public MessageCenterAdapter(Context mContext, List<MessageCenterBean.MessagesBean> mCasesEntities, Boolean ifIsDesiner, int right_wid) {
+        mCheckeds = new HashSet<>();
         this.mContext = mContext;
         this.mCasesEntities = mCasesEntities;
         this.ifIsDesiner = ifIsDesiner;
+        this.right_wid = right_wid;
+    }
+
+    public Set<Integer> getmCheckeds() {
+        return mCheckeds;
+    }
+
+    public void setEditor(boolean status) {
+        isClickEditor = status;
+    }
+
+    public boolean getEditor() {
+        return isClickEditor;
+    }
+
+    public boolean getClickedAll() {
+        return isClickedAll;
+    }
+
+    public void setClickedAll(boolean clickedAll) {
+        isClickedAll = clickedAll;
     }
 
     @Override
@@ -54,7 +91,7 @@ public class MessageCenterAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         MyHolder myHolder;
         if (convertView == null) {
@@ -63,11 +100,14 @@ public class MessageCenterAdapter extends BaseAdapter {
             myHolder.item_msg_content = (TextView) convertView.findViewById(R.id.tv_msg_content);
             myHolder.tv_msg_title = (TextView) convertView.findViewById(R.id.tv_msg_title);
             myHolder.tv_msg_date = (TextView) convertView.findViewById(R.id.tv_msg_date);
+            myHolder.checkBox = (CheckBox) convertView.findViewById(R.id.item_msg_checkout);
+            myHolder.item_left = (View) convertView.findViewById(R.id.item_relayout_left);
+            myHolder.item_right = (View) convertView.findViewById(R.id.item_relayout_right);
             convertView.setTag(myHolder);
         } else {
             myHolder = (MyHolder) convertView.getTag();
         }
-        MessageCenterBean.MessagesBean messagesBean = mCasesEntities.get(position);
+        final MessageCenterBean.MessagesBean messagesBean = mCasesEntities.get(position);
 
         String body = messagesBean.getBody();
         String title = messagesBean.getTitle();
@@ -78,11 +118,10 @@ public class MessageCenterAdapter extends BaseAdapter {
             Long aLong = Long.valueOf(sent_time);
             timeMY = DateUtil.showDate(aLong);
         }
-
         title = TextUtils.isEmpty(title) ? "消息中心" : title;
 
-        if(title.length()>12){
-            title = title.substring(0,12)+"...";
+        if (title.length() > 12) {
+            title = title.substring(0, 12) + "...";
         }
         myHolder.tv_msg_title.setText(title);
         myHolder.tv_msg_date.setText(timeMY);
@@ -92,16 +131,68 @@ public class MessageCenterAdapter extends BaseAdapter {
 
             Log.d("test", messageCenterBody.toString());
             if (ifIsDesiner) {
-                myHolder.item_msg_content.setText(messageCenterBody.getFor_designer().replace("&gt;",">"));
+                myHolder.item_msg_content.setText(messageCenterBody.getFor_designer().replace("&gt;", ">"));
             } else {
-                myHolder.item_msg_content.setText(messageCenterBody.getFor_consumer().replace("&gt;",">"));
+                myHolder.item_msg_content.setText(messageCenterBody.getFor_consumer().replace("&gt;", ">"));
             }
         } else {
-            myHolder.item_msg_content.setText(body.replace("&gt;",">"));
+            myHolder.item_msg_content.setText(body.replace("&gt;", ">"));
         }
+        // 设置添加消息左滑删除功能
+        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        myHolder.item_left.setLayoutParams(lp1);
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(right_wid, LinearLayout.LayoutParams.MATCH_PARENT);
+        myHolder.item_right.setLayoutParams(lp2);
 
+        myHolder.item_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "左侧", Toast.LENGTH_SHORT).show();
+            }
+        });
+        final View finalConvertView = convertView;
+        myHolder.item_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                deleteMessage.deleteOneMsg(position, messagesBean, finalConvertView);
+
+            }
+        });
+//        myHolder.checkBox.setChecked(mCheckeds.contains(position));
+//        //checkBox设置
+//        if (!isClickEditor) {
+//            myHolder.checkBox.setVisibility(View.GONE);
+//        } else {
+//            myHolder.checkBox.setVisibility(View.VISIBLE);
+//        }
+//        //checkBox监听
+//        myHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    mCheckeds.add(position);
+//                } else {
+//                    mCheckeds.remove(position);
+//                }
+//
+//            }
+//        });
 
         return convertView;
+    }
+
+    public void addAll() {
+        for (int i = 0; i < mCasesEntities.size(); i++) {
+            mCheckeds.add(i);
+        }
+        this.notifyDataSetChanged();
+    }
+
+    public void clearChose() {
+        mCheckeds.clear();
+        this.notifyDataSetChanged();
     }
 
     class MyHolder {
@@ -109,7 +200,19 @@ public class MessageCenterAdapter extends BaseAdapter {
         private TextView tv_msg_date;
         private TextView tv_msg_title;
         private TextView item_msg_content;
+        private View item_left;
 
+        private CheckBox checkBox;
+        private View item_right;
+
+    }
+
+    public void setDeleteMessageListener(DeleteMessage deleteMessage) {
+        this.deleteMessage = deleteMessage;
+    }
+
+    public interface DeleteMessage {
+        public void deleteOneMsg(int position, MessageCenterBean.MessagesBean messagesBean,View convertView);
     }
 
 }
