af0aa12f77dc3faa37ef7c02d8a68e1c8dd2247b
LtdLuchongbin <chongbin.lu@autodesk.com>
2016-10-08 16:06:02 +0800

    [BUG][00000][去掉android不建议永的枚举][Reviewed by：luzhipeng]

--- shared/src/main/java/com/autodesk/shejijia/shared/components/common/appglobal/Constant.java
+++ shared/src/main/java/com/autodesk/shejijia/shared/components/common/appglobal/Constant.java
@@ -40,6 +40,21 @@ public class Constant {
     }
 
     /**
+     * 登录用户数据用到的key
+     */
+    public static class FragmentEnum {
+
+        public static final int ZERO = 0;
+        public static final int ONE = 1;
+        public static final int TWO = 2;
+        public static final int THREE = 3;
+        public static final int FOUR = 4;
+        public static final int FIVE = 5;
+
+    }
+
+
+    /**
      * Flow pass parameter .
      */
     public static class ProjectMaterialKey {
