404d25558d62a9e67596d79d7a4dd2eed24a1bd0
liuhea <he.liu@leediancn.com>
2016-10-11 14:13:04 +0800

    [Bug][DP-6280][重新修复Android-我的项目 消费者我的项目-有些显示暂无数据][Reviewed by:chognbin.lu]

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/resdecoration/adapter/DecorationOrdinaryDelegate.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/resdecoration/adapter/DecorationOrdinaryDelegate.java
@@ -84,7 +84,7 @@ public class DecorationOrdinaryDelegate implements ItemViewDelegate<DecorationNe
 //        final String thread_id = decorationNeedsListBean.getBeishu_thread_id();
         String custom_string_status = decorationNeedsListBean.getCustom_string_status();
 
-        holder.setText(R.id.tv_decoration_name, UIUtils.getNoDataIfEmpty(contacts_name) + "/" + UIUtils.getNoDataIfEmpty(contacts_name));
+        holder.setText(R.id.tv_decoration_name, UIUtils.getNoDataIfEmpty(contacts_name) + "/" + UIUtils.getNoDataIfEmpty(community_name));
         holder.setText(R.id.tv_decoration_needs_id, decorationNeedsListBean.getNeeds_id());
 
         String house_type = decorationNeedsListBean.getHouse_type();
