f74ed0dd321f56dea74b444cdf3bf81efff4b530
liuhea <he.liu@leediancn.com>
2016-10-24 17:01:27 +0800

    [Task][MP-1381][新建推荐清单页面类][Reviewed by:xueqiu.dong]

---
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/recommend/activity/RecommendListDetailActivity.java
@@ -0,0 +1,21 @@
+package com.autodesk.shejijia.consumer.personalcenter.recommend.activity;
+
+
+import com.autodesk.shejijia.consumer.R;
+import com.autodesk.shejijia.shared.framework.activity.NavigationBarActivity;
+
+/**
+ * 清单页面:空白页面及编辑清单页面
+ *
+ * @author liuhea
+ *         created at 16-10-24
+ */
+
+public class RecommendListDetailActivity extends NavigationBarActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_recommend_list_detail;
+    }
+
+}
