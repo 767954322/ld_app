d6f9b47f8da4a825fb45486186e00660532869e2
t_xuz <zhebao.xu@autodesk.com>
2016-10-27 09:59:04 +0800

    ［shared］logutils 修改

--- shared/src/main/java/com/autodesk/shejijia/shared/components/common/utility/LogUtils.java
+++ shared/src/main/java/com/autodesk/shejijia/shared/components/common/utility/LogUtils.java
@@ -15,8 +15,7 @@ public final class LogUtils {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
 
-//    public static boolean isDebug = AdskApplication.getInstance().isDebug();// 是否需要打印bug，可以在application的onCreate函数里面初始化
-    public static boolean isDebug = true;
+    public static boolean isDebug = AdskApplication.getInstance().isDebug();// 是否需要打印bug，可以在application的onCreate函数里面初始化
     private static final String TAG = "AutoDesk--Log--";
 
     // 下面四个是默认tag的函数
