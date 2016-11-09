12485990d0255c1dd07d97869e34f598cea1c3bd
HarryAi <jiabin.ai@autodesk.com>
2016-11-09 16:07:09 +0800

    [enterprise]在闪屏页上添加角色登入的判断

--- enterprise/src/main/java/com/autodesk/shejijia/enterprise/MPSplashActivity.java
+++ enterprise/src/main/java/com/autodesk/shejijia/enterprise/MPSplashActivity.java
@@ -1,7 +1,11 @@
 package com.autodesk.shejijia.enterprise;
 
+import android.text.TextUtils;
+
+import com.autodesk.shejijia.shared.components.common.appglobal.ConstructionConstants;
 import com.autodesk.shejijia.shared.components.common.tools.login.RegisterOrLoginActivity;
 import com.autodesk.shejijia.shared.components.common.utility.UserInfoUtils;
+import com.autodesk.shejijia.shared.components.form.ui.activity.ScanQrCodeActivity;
 import com.autodesk.shejijia.shared.framework.activity.SplashActivity;
 
 
@@ -9,10 +13,17 @@ public class MPSplashActivity extends SplashActivity {
     @Override
     protected Class getNextActivityToLaunch() {
 
-        if (UserInfoUtils.getToken(this) != null) {
-            return EnterpriseHomeActivity.class;
-        } else {
+        String memberType = UserInfoUtils.getMemberType(this);
+        if(TextUtils.isEmpty(memberType)) {
             return RegisterOrLoginActivity.class;
+        } else {
+           switch(memberType) {
+               case ConstructionConstants.INSPECTOR_TYPE:
+                   return ScanQrCodeActivity.class;
+               default:
+                   return EnterpriseHomeActivity.class;
+           }
         }
+
     }
 }
