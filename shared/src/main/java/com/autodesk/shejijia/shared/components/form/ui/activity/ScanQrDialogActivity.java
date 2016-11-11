faff8a23a0ad1ec522df6063560014f6e5cba8c1
HarryAi <jiabin.ai@autodesk.com>
2016-11-11 18:09:38 +0800

    [enterprise][form]改写扫码弹框继承BaseActivity

--- shared/src/main/java/com/autodesk/shejijia/shared/components/form/ui/activity/ScanQrDialogActivity.java
+++ shared/src/main/java/com/autodesk/shejijia/shared/components/form/ui/activity/ScanQrDialogActivity.java
@@ -2,51 +2,50 @@ package com.autodesk.shejijia.shared.components.form.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.autodesk.shejijia.shared.R;
+import com.autodesk.shejijia.shared.framework.activity.BaseActivity;
+
 
 /**
  * Created by t_aij on 16/11/1.
  */
 
-public class ScanQrDialogActivity extends AppCompatActivity implements View.OnClickListener {
+public class ScanQrDialogActivity extends BaseActivity implements View.OnClickListener {
 
     private TextView mTitle;
     private TextView mContent;
     private String mFormat;
     private String mError;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanqr_dialog);
-        initIntent();
-        initView();
-        initData();
 
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_scanqr_dialog;
     }
 
-    private void initIntent() {
+    @Override
+    protected void initView() {
+        mTitle = (TextView) findViewById(R.id.tv_title);
+        mContent = (TextView) findViewById(R.id.tv_content);
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
         Intent intent = getIntent();
         mFormat = intent.getStringExtra("format");
         mError = intent.getStringExtra("error");
-    }
 
-    private void initView() {
-        mTitle = (TextView) findViewById(R.id.tv_title);
-        mContent = (TextView) findViewById(R.id.tv_content);
-
-        findViewById(R.id.btn_submit).setOnClickListener(this);
-        findViewById(R.id.btn_cancel).setOnClickListener(this);
-    }
-
-    private void initData() {
         mTitle.setText("提示");
         mContent.setText(mError == null ? mFormat : mError);
+    }
+
+    @Override
+    protected void initListener() {
+        findViewById(R.id.btn_submit).setOnClickListener(this);
+        findViewById(R.id.btn_cancel).setOnClickListener(this);
     }
 
     @Override
