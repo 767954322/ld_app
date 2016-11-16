3ddcbecb6db0bce3f0c28e6f7667e594a9c70f4a
t_xuz <zhebao.xu@autodesk.com>
2016-11-16 18:22:19 +0800

    [enterprise]添加一些drawable文件

---
+++ shared/src/main/java/com/autodesk/shejijia/shared/components/nodeprocess/bean/TaskListBean.java
@@ -0,0 +1,8 @@
+package com.autodesk.shejijia.shared.components.nodeprocess.bean;
+
+/**
+ * Created by t_xuz on 11/16/16.
+ */
+
+public class TaskListBean {
+}
---
+++ shared/src/main/java/com/autodesk/shejijia/shared/components/nodeprocess/ui/widgets/progressbar/ProgressbarAdapter.java
@@ -0,0 +1,8 @@
+package com.autodesk.shejijia.shared.components.nodeprocess.ui.widgets.progressbar;
+
+/**
+ * Created by t_xuz on 11/15/16.
+ */
+
+public class ProgressbarAdapter {
+}
