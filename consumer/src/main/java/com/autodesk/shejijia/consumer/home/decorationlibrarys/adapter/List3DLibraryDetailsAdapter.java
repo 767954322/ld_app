439426d5f81a18203b0ca5a01aa7ef64b042dfa1
yangxuewuWillson <xuewu.yang@leediancn.com>
2016-11-17 14:32:48 +0800

    [bug][DP-6824	][ 缺陷 【Android-3D案例】案例中默认图下方没有“暂无图片”的提示][reviewed by lizhipeng]

--- consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationlibrarys/adapter/List3DLibraryDetailsAdapter.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/home/decorationlibrarys/adapter/List3DLibraryDetailsAdapter.java
@@ -2,6 +2,7 @@ package com.autodesk.shejijia.consumer.home.decorationlibrarys.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -9,13 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.autodesk.shejijia.consumer.R;
 import com.autodesk.shejijia.consumer.home.decorationlibrarys.activity.CaseLibraryDetailActivity;
 import com.autodesk.shejijia.consumer.home.decorationlibrarys.activity.CaseLibraryRoamingWebView;
 import com.autodesk.shejijia.shared.components.common.appglobal.Constant;
-import com.autodesk.shejijia.shared.components.common.utility.ImageUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import java.io.Serializable;
 import java.util.List;
@@ -47,31 +50,53 @@ public class List3DLibraryDetailsAdapter extends RecyclerView.Adapter<RecyclerVi
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-        List3dViewVH viewVH = (List3dViewVH) holder;
+        final List3dViewVH viewVH = (List3dViewVH) holder;
         if (!TextUtils.isEmpty(imageLists.get(position))) {
             if (mType.equals("4")) {
-                String maYouUrl = imageLists.get(position);
-                if (maYouUrl.equals("drawable")) {
-                    ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.default_3d_details);
-                    // ImageUtils.loadFileImage(viewVH.m3DetailsImage,"drawable://"+R.drawable.roaming_figure);
-                    viewVH.m3DetailsImage.setVisibility(View.GONE);
-                }else if (maYouUrl.contains("COVERANDLINK")){
-                    String  cover=maYouUrl.substring(0,maYouUrl.indexOf("COVERANDLINK"));
-                    String  link=maYouUrl.substring(maYouUrl.indexOf("COVERANDLINK")+12);
-                    if (!cover.equals("HD.jpg")){
-                        ImageUtils.loadFileImage(viewVH.m3DDetailsImage, cover);
-                    }else {
-                        ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.images_3d);
-                    }
-                    ImageUtils.loadFileImage(viewVH.m3DetailsImage, "drawable://" + R.drawable.roaming_figure);
-                    viewVH.m3DetailsImage.setVisibility(View.VISIBLE);
-                }else {
-                    ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.images_3d);
-                    ImageUtils.loadFileImage(viewVH.m3DetailsImage, "drawable://" + R.drawable.roaming_figure);
-                    viewVH.m3DetailsImage.setVisibility(View.VISIBLE);
-                }
+//                String maYouUrl = imageLists.get(position);
+//                if (maYouUrl.equals("drawable")) {
+//                    ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.default_3d_details);
+//                    // ImageUtils.loadFileImage(viewVH.m3DetailsImage,"drawable://"+R.drawable.roaming_figure);
+//                    viewVH.m3DetailsImage.setVisibility(View.GONE);
+//                } else if (maYouUrl.contains("COVERANDLINK")) {
+//                    String cover = maYouUrl.substring(0, maYouUrl.indexOf("COVERANDLINK"));
+//                    String link = maYouUrl.substring(maYouUrl.indexOf("COVERANDLINK") + 12);
+//                    if (!cover.equals("HD.jpg")) {
+//                        ImageUtils.loadFileImage(viewVH.m3DDetailsImage, cover);
+//                    } else {
+//                        ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.images_3d);
+//                    }
+//                    ImageUtils.loadFileImage(viewVH.m3DetailsImage, "drawable://" + R.drawable.roaming_figure);
+//                    viewVH.m3DetailsImage.setVisibility(View.VISIBLE);
+//                } else {
+//                    ImageUtils.loadFileImage(viewVH.m3DDetailsImage, "drawable://" + R.drawable.images_3d);
+//                    ImageUtils.loadFileImage(viewVH.m3DetailsImage, "drawable://" + R.drawable.roaming_figure);
+//                    viewVH.m3DetailsImage.setVisibility(View.VISIBLE);
+//                }
             } else {
-                ImageLoader.getInstance().displayImage(imageLists.get(position), viewVH.m3DDetailsImage);
+//                ImageLoader.getInstance().displayImage(imageLists.get(position), viewVH.m3DDetailsImage,);
+                ImageLoader.getInstance().loadImage(imageLists.get(position), new ImageLoadingListener() {
+                    @Override
+                    public void onLoadingStarted(String imageUri, View view) {
+
+                    }
+
+                    @Override
+                    public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+                    }
+
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                        viewVH.m3DDetailsImage.setImageBitmap(loadedImage);
+                        viewVH.tv_no_image.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onLoadingCancelled(String imageUri, View view) {
+
+                    }
+                });
             }
 
         }
@@ -83,7 +108,7 @@ public class List3DLibraryDetailsAdapter extends RecyclerView.Adapter<RecyclerVi
                 if (mType.equals("4")) {
                     String webUrl = imageLists.get(position);
                     if (!webUrl.equals("drawable")) {
-                        String  link=webUrl.substring(webUrl.indexOf("COVERANDLINK")+12);
+                        String link = webUrl.substring(webUrl.indexOf("COVERANDLINK") + 12);
                         Intent intent = new Intent(mContext, CaseLibraryRoamingWebView.class);
                         intent.putExtra("roaming", link);
                         mContext.startActivity(intent);
@@ -112,11 +137,13 @@ public class List3DLibraryDetailsAdapter extends RecyclerView.Adapter<RecyclerVi
     private class List3dViewVH extends RecyclerView.ViewHolder {
         public ImageView m3DDetailsImage;
         public ImageView m3DetailsImage;
+        public TextView tv_no_image;
 
         public List3dViewVH(View itemView) {
             super(itemView);
             m3DDetailsImage = (ImageView) itemView.findViewById(R.id.image_3d_photo);
             m3DetailsImage = (ImageView) itemView.findViewById(R.id.image_quanjing);
+            tv_no_image = (TextView) itemView.findViewById(R.id.tv_no_image);
         }
     }
 }
