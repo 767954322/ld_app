9cf2e02e8f683ce343eb3605c15817516ceecded
jinlong.zhou <jinlong.zhou@leediancn.com>
2016-12-05 09:41:26 +0800

    [Bug][MP-1873][修改店面地址,去掉空字段显示问题][reviewed by he.liu]

--- consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/recommend/adapter/StoreLocationAdapter.java
+++ consumer/src/main/java/com/autodesk/shejijia/consumer/personalcenter/recommend/adapter/StoreLocationAdapter.java
@@ -25,7 +25,7 @@ public class StoreLocationAdapter extends CommonAdapter<MallAddressEntity.MallAd
     public void convert(CommonViewHolder holder, MallAddressEntity.MallAddressesBean entity) {
         holder.setText(R.id.tv_category_name, entity.getStorefront_name());
         holder.setText(R.id.tv_office_hours, entity.getBusiness_hours());
-        holder.setText(R.id.tv_mall_address, entity.getDetailed_address());
+        holder.setText(R.id.tv_mall_address, entity.getStorefront_address());
         holder.setText(R.id.tv_booth_address, entity.getDetailed_address());
         holder.setText(R.id.tv_route_line, entity.getRide_route());
         holder.setText(R.id.tv_mall_mobile, entity.getBooth_phone());
