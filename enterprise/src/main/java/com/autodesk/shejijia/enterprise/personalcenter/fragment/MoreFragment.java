1b104175cce8e228523bc92f77c9414455d2fcd5
LtdLuchongbin <chongbin.lu@autodesk.com>
2016-12-15 16:33:59 +0800

    [enterprise][完成清楚缓存的功能]

--- enterprise/src/main/java/com/autodesk/shejijia/enterprise/personalcenter/fragment/MoreFragment.java
+++ enterprise/src/main/java/com/autodesk/shejijia/enterprise/personalcenter/fragment/MoreFragment.java
@@ -2,24 +2,28 @@ package com.autodesk.shejijia.enterprise.personalcenter.fragment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.autodesk.shejijia.enterprise.R;
 import com.autodesk.shejijia.enterprise.personalcenter.activity.AboutActivity;
+import com.autodesk.shejijia.shared.components.common.uielements.CustomProgress;
+import com.autodesk.shejijia.shared.components.common.utility.CommonUtils;
+import com.autodesk.shejijia.shared.components.common.utility.DataCleanManager;
+import com.autodesk.shejijia.shared.components.common.utility.MPFileUtility;
+import com.autodesk.shejijia.shared.components.common.utility.UIUtils;
 import com.autodesk.shejijia.shared.framework.fragment.BaseConstructionFragment;
 import com.autodesk.shejijia.enterprise.common.utils.LoginUtils;
 import com.autodesk.shejijia.shared.components.common.utility.ToastUtils;
-import com.autodesk.shejijia.shared.components.common.tools.login.RegisterOrLoginActivity;
 import com.autodesk.shejijia.shared.components.common.utility.BackGroundUtils;
+
+import java.io.File;
 
 /**
  * Created by t_xuz on 8/30/16.
@@ -31,7 +35,7 @@ public class MoreFragment extends BaseConstructionFragment implements View.OnCli
     private TextView mClearCache;
     private TextView mAboutApp;
     private TextView mLogoutApp;
-    private PopupWindow bottomPopUp;
+    private PopupWindow mBottomPopUp;
 
     public static MoreFragment newInstance() {
         return new MoreFragment();
@@ -79,56 +83,81 @@ public class MoreFragment extends BaseConstructionFragment implements View.OnCli
     }
 
     private void initBottomPopup() {
+        String cacheSize = getCacheSize();
         View contentView = LayoutInflater.from(mContext).inflate(R.layout.popup_clear_cache_dialog, null);
         TextView mCacheSize = (TextView) contentView.findViewById(R.id.tv_cache_size);
         TextView mCacheConfirm = (TextView) contentView.findViewById(R.id.tv_cache_confirm);
         TextView mCacheCancel = (TextView) contentView.findViewById(R.id.tv_cache_cancel);
-        mCacheSize.setText("清除本地缓存数据(包括图片和语音缓存)");
+        mCacheSize.setText(UIUtils.getString(R.string.clear_cache)+cacheSize+")");
         mCacheSize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ToastUtils.showShort(mContext, "预约时间已录入!");
+
             }
         });
         mCacheConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomPopUp.dismiss();
+                clearCache();
             }
         });
         mCacheCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomPopUp.dismiss();
+                mBottomPopUp.dismiss();
             }
         });
 
         contentView.setFocusable(true);
         contentView.setFocusableInTouchMode(true);
-        bottomPopUp = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        bottomPopUp.setFocusable(true);
-        bottomPopUp.setOutsideTouchable(false);
+        mBottomPopUp = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mBottomPopUp.setFocusable(true);
+        mBottomPopUp.setOutsideTouchable(false);
         contentView.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
                 if (keyCode == KeyEvent.KEYCODE_BACK) {
-
-                    bottomPopUp.dismiss();
+                    mBottomPopUp.dismiss();
                     return true;
                 }
                 return false;
             }
         });
-        bottomPopUp.setOnDismissListener(new PopupWindow.OnDismissListener() {
+        mBottomPopUp.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
                 BackGroundUtils.dimWindowBackground(mContext, 0.7f, 1.0f);
             }
         });
-        View view = (((Activity) mContext).findViewById(android.R.id.content)).getRootView();
-        bottomPopUp.setAnimationStyle(R.style.pop_bottom_animation);
+        View view = ((mContext).findViewById(android.R.id.content)).getRootView();
+        mBottomPopUp.setAnimationStyle(R.style.pop_bottom_animation);
         BackGroundUtils.dimWindowBackground(mContext, 1.0f, 0.7f);
-        bottomPopUp.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+        mBottomPopUp.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+    }
+    private void clearCache(){
+        CustomProgress.show(getActivity(), "", false, null);
+        CommonUtils.clearAppCache(getActivity());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                CustomProgress.cancelDialog();
+                ToastUtils.showLong(getActivity(),UIUtils.getString(R.string.successful));
+            }
+        }, 1000);
+        mBottomPopUp.dismiss();
+    }
+    private String getCacheSize() {
+        String cacheSize ="";
+        File cacheDir = getActivity().getCacheDir();
+        try {
+            double mpCachesize = DataCleanManager.getFolderSize(MPFileUtility.getCacheRootDirectoryHandle(getActivity()));
+            double othercachesize = DataCleanManager.getFolderSize(cacheDir);
+            cacheSize = DataCleanManager.getFormatSize(mpCachesize + othercachesize);
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+        return cacheSize;
     }
 
 }
